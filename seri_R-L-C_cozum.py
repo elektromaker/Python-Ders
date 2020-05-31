05d23defb3c9a112013875c4bb7f554fb73b90af
elektromaker <gedikbas@gmail.com>
2020-05-31 21:14:25 +0300

    Update seri_R-L-C_cozum.py

--- seri_R-L-C_cozum.py
+++ seri_R-L-C_cozum.py
@@ -1,3 +1,12 @@
+####################################
+#Yazar: Elekktromaker.com
+#Açıklama:
+#Bu kod Meslek lisesindeki elektrik-elektronik esasları dersindeki Seri RLC develer konusu için hazırlanmıştır.
+#İdeal bobin ve ideal kondansatör kullanılan (akım ve gerilim arasında doksan derece faz farkı bulunan elemanlar)
+#seri RLC devrelerinde; Direnç, bobin, kondansatör, Frekans ve gerilim değerleri verilerek;
+#Endüktif reaktans, kapasitif reaktans, Empedans, Akım, Elemanlar üzerine düşen gerilimler ve faz açısının hesaplanmasını yapan
+#python kodarıdır.
+##################################### 
 import math
 class SeriRLC:
     
@@ -14,11 +23,11 @@ class SeriRLC:
     def kapasitifReaktans(self):
         return 10**(6)/(2*3.14*60*self.C)
 
-    def enduktans(self):
+    def empedans(self):
         return math.sqrt(self.R**2 + (kapReaktans-endReaktans)**2)
 
     def akim(self):
-        return (self.V/Enduktans)*1000
+        return (self.V/Empedans)*1000
 
     def direncGerilimi(self):
         return (Akim/1000)*self.R
@@ -35,7 +44,7 @@ class SeriRLC:
 hesapla = SeriRLC()
 endReaktans = hesapla.enduktifReaktans()
 kapReaktans = hesapla.kapasitifReaktans()
-Enduktans = hesapla.enduktans()
+Empedans = hesapla.empedans()
 Akim = hesapla.akim()
 DirencVolt = hesapla.direncGerilimi()
 BobinVolt = hesapla.bobinGerilimi()
@@ -43,7 +52,7 @@ KondansatorVolt = hesapla.kondansatorGerilim()
 FazAci = hesapla.fazAcisi()
 print("Endüktif Reaktans: " + str(round(endReaktans,2)) + " ohm")
 print("Kapasitif Reaktans: " + str(round(kapReaktans,2)) + " ohm")
-print("Endüktans: " + str(round(Enduktans,2)) + " ohm")
+print("Empedans: " + str(round(Empedans,2)) + " ohm")
 print("Akım(miliAmper): " + str(round(Akim,2)) + " mA")
 print("Direnç üzerine düşen gerilim: " + str(round(DirencVolt,2)) + " V")
 print("Bobin üzerine düşen gerilim: " + str(round(BobinVolt,2)) + " V")
